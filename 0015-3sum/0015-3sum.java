1fd8e53bda3f8ccfd4d045aeabcad34672844604
Anuj Mourya <161029725+Anuj1143@users.noreply.github.com>
2025-04-19 12:04:36 +0530

    Time: 873 ms (9.53%), Space: 52.3 MB (27.22%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,38 +1,31 @@
 class Solution {
-     List<List<Integer>> result=new ArrayList<>();
-    void TwoSum(int[] nums, int target, int i, int j){
-        while(i<j){
-            if(nums[i]+nums[j]>target){
-                j--;
-            }
-            else if(nums[i]+nums[j]<target){
-               i++;
-            }
-            else{
-                while(i<j && nums[i]==nums[i+1]) i++;
-                while(i<j && nums[j]==nums[j-1]) j--;
-                result.add(Arrays.asList(-target, nums[i], nums[j]));
-                i++;
-                j--;
-
-            }
-        }
-    }
     public List<List<Integer>> threeSum(int[] nums) {
         int n=nums.length;
-        if(nums.length<3){
-            return new ArrayList<>();
-        }
-       
         Arrays.sort(nums);
-        for(int i=0; i<=n-3; i++){
-            if(i!=0 && nums[i]==nums[i-1]){
-                continue;
+
+        if(nums==null || n<3 ) return new ArrayList<>();
+
+        Set<List<Integer>>result=new HashSet<>();
+
+        for(int i=0; i<n-2; i++){
+            int left=i+1;
+            int right=n-1;
+            while(left<right){
+                int sum=nums[i]+nums[left]+nums[right];
+                if(sum==0){
+
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    left++;
+                    right--;
+                }
+                else if(sum<0){
+                    left++;
+                }
+                else
+                    right--;
             }
-            int n1=nums[i];
-            int target=-n1;
-            TwoSum(nums, target, i+1, n-1);
         }
-        return result;
+
+        return new ArrayList<>(result);
     }
 }
