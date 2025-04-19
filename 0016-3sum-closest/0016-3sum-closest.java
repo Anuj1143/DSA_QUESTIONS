b112244abed0fced931147e8bcf1593dcb2325be
Anuj Mourya <161029725+Anuj1143@users.noreply.github.com>
2025-04-19 11:29:41 +0530

    Time: 16 ms (23.31%), Space: 43.2 MB (76.69%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.java
+++ 0016-3sum-closest/0016-3sum-closest.java
@@ -1,28 +1,28 @@
 class Solution {
     public int threeSumClosest(int[] nums, int target) {
-        int n=nums.length;
+        int n= nums.length;
         Arrays.sort(nums);
-        int closestSum=Integer.MAX_VALUE;
+        int closeSum=Integer.MAX_VALUE;
         for(int k=0; k<n-2; k++){
+
             int i=k+1;
             int j=n-1;
             while(i<j){
                 int sum=nums[k]+nums[i]+nums[j];
-                 if (sum == target) {
+                if(sum==target){
+
                     return sum;
                 }
-                if(Math.abs(target-sum)<Math.abs(target-closestSum))
-                    closestSum=sum;
-                
-                if(sum<target)
+
+                if(Math.abs(target-sum)<Math.abs(target-closeSum))
+                    closeSum=sum;
+                if(sum<target){
                     i++;
-                
+                }
                 else
                     j--;
-                
             }
         }
-        return closestSum;
-
+        return closeSum;
     }
 }
