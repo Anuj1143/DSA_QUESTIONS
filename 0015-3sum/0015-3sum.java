c68bd17e05e3e0fcaffae69f764b83c06d48fbc7
Anuj Mourya <161029725+Anuj1143@users.noreply.github.com>
2025-05-29 10:34:54 +0530

    Time: 28 ms (80.24%), Space: 51.2 MB (92.28%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -4,7 +4,7 @@ class Solution {
         Arrays.sort(nums);
         for(int i=0; i<nums.length; i++){
             if(i>0&& nums[i]==nums[i-1])continue;
-            int j=1;
+            int j=i+1;
             int k=nums.length-1;
             while(j<k){
                 int sum=nums[i]+nums[j]+nums[k];
@@ -15,8 +15,10 @@ class Solution {
                 }
                 else{
                     ans.add(Arrays.asList(nums[i], nums[j], nums[k]));
-                    while(j<k&& nums[j]==nums[j+1])j++;
-                    while(j<k && nums[k]==nums[k-1])k--;
+                    j++;
+                    k--;
+                    while(j<k&& nums[j]==nums[j-1])j++;
+                    while(j<k && nums[k]==nums[k+1])k--;
                 }
             }
         }
