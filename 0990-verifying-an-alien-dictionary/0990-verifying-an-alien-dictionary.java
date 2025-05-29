10909175481e516095dca868f6ce780958eff2c2
Anuj Mourya <161029725+Anuj1143@users.noreply.github.com>
2025-05-29 10:46:27 +0530

    Time: 0 ms (100%), Space: 41.9 MB (85.78%) - LeetHub

--- 0990-verifying-an-alien-dictionary/0990-verifying-an-alien-dictionary.java
+++ 0990-verifying-an-alien-dictionary/0990-verifying-an-alien-dictionary.java
@@ -4,14 +4,16 @@ class Solution {
         int seq=0;
         for(char ch:order.toCharArray()){
             mapping[ch-'a']=seq++;
+
         }
         for(int i=0; i<words.length-1; i++){
             String curr=words[i];
             String next=words[i+1];
             int len=Math.min(curr.length(), next.length());
-            if(curr.startsWith(next) && curr.length()> next.length()){
+            if(curr.startsWith(next)&&curr.length()>next.length()){
                 return false;
             }
+
             for(int j=0; j<len; j++){
                 if(mapping[curr.charAt(j)-'a']>mapping[next.charAt(j)-'a']){
                     return false;
@@ -22,5 +24,6 @@ class Solution {
             }
         }
         return true;
+    
     }
 }
