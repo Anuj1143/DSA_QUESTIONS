6c0e4b96ffc47b36ac790ae2edd1afd2495de340
Anuj Mourya <161029725+Anuj1143@users.noreply.github.com>
2025-06-16 15:29:29 +0530

    Time: 7 ms (97.68%), Space: 43 MB (56.16%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,31 +1,46 @@
 class Solution {
-    boolean solve(String s, int i, int j){
-        if(i>=j){
-            return true;
-        }
-        if(s.charAt(i)==s.charAt(j)){
-            return solve(s, i+1, j-1);
+    public static String longestPalindrome(String s) {
+        if (s == null || s.length() == 0) return "";
 
+        // Transform string: add # between characters
+        StringBuilder t = new StringBuilder("^");
+        for (char c : s.toCharArray()) {
+            t.append("#").append(c);
         }
-        return false;
-    }
-    public String longestPalindrome(String s) {
-        int n=s.length();
-        int maxLen=Integer.MIN_VALUE;
-        int sp=0;
-        for(int i=0; i<n; i++){
-            for(int j=i;j<n; j++){
-                if(solve(s, i, j)==true){
-                    if(j-i+1>maxLen){
-                        maxLen=j-i+1;
-                        sp=i;
-                    }
-                }
+        t.append("#$");
+
+        int n = t.length();
+        int[] P = new int[n];
+        int center = 0, right = 0;
+
+        for (int i = 1; i < n - 1; i++) {
+            int mirror = 2 * center - i;
+
+            if (i < right)
+                P[i] = Math.min(right - i, P[mirror]);
+
+            // Expand around center i
+            while (t.charAt(i + P[i] + 1) == t.charAt(i - P[i] - 1)) {
+                P[i]++;
+            }
+
+            // Update center and right
+            if (i + P[i] > right) {
+                center = i;
+                right = i + P[i];
             }
         }
 
-        return s.substring(sp,sp+ maxLen);
+        // Find max length and center
+        int maxLen = 0, centerIndex = 0;
+        for (int i = 1; i < n - 1; i++) {
+            if (P[i] > maxLen) {
+                maxLen = P[i];
+                centerIndex = i;
+            }
+        }
 
-        
+        int start = (centerIndex - maxLen) / 2;
+        return s.substring(start, start + maxLen);
     }
 }
