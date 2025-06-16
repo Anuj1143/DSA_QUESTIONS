1134595bea56c732b9193cea3cac643c2755c4b0
Anuj Mourya <161029725+Anuj1143@users.noreply.github.com>
2025-06-16 17:17:21 +0530

    Time: 1 ms (62.17%), Space: 46.1 MB (81.79%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,8 +1,9 @@
 class Solution {
     public int trap(int[] arr) {
         int n=arr.length;
-        int[]prefix=new int[n];
-        int[]suffix=new int[n];
+        int prefix[]=new int[n];
+        int suffix[]=new int[n];
+
         prefix[0]=arr[0];
         for(int i=1; i<n; i++){
             prefix[i]=Math.max(arr[i], prefix[i-1]);
@@ -12,13 +13,15 @@ class Solution {
             suffix[i]=Math.max(arr[i], suffix[i+1]);
         }
         int total=0;
-        for(int i=0; i<n;i++){
-            int leftmax=prefix[i];
-            int rightmax=suffix[i];
-            if(arr[i]<leftmax && arr[i]<suffix[i]){
-                total+=Math.min(leftmax, rightmax)-arr[i];
-            }
+        for(int i=0; i<n; i++){
+        int leftMax=prefix[i];
+        int rightMax=suffix[i];
+        if(arr[i]<leftMax && arr[i]<rightMax){
+            total+=Math.min(leftMax, rightMax)-arr[i];
         }
+        }
+
         return total;
     }
+
 }
