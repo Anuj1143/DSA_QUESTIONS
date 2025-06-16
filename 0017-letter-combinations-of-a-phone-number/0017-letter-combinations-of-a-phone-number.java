c520f83c250794f98dbaee3e2a983c8bab3a6eda
Anuj Mourya <161029725+Anuj1143@users.noreply.github.com>
2025-06-16 18:32:33 +0530

    Time: 0 ms (100%), Space: 41.9 MB (94.71%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,36 +1,37 @@
 class Solution {
     List<String>result=new ArrayList<>();
-    public void solve(int idx, String digits, StringBuilder temp, Map<Character,String>mp){
+    public void solve(int idx, String digits, StringBuilder sb, Map<Character, String>mp){
         if(idx>=digits.length()){
-            result.add(temp.toString());
+            result.add(sb.toString());
             return;
+
         }
         char ch=digits.charAt(idx);
         String str=mp.get(ch);
         for(int i=0; i<str.length(); i++){
-            temp.append(str.charAt(i));
-            solve(idx+1, digits,temp, mp);
-            temp.deleteCharAt(temp.length()-1);
+            sb.append(str.charAt(i));
+            solve(idx+1, digits, sb, mp);
+            sb.deleteCharAt(sb.length()-1);
         }
     }
     public List<String> letterCombinations(String digits) {
+        result=new ArrayList<>();
         if(digits.length()==0){
             return new ArrayList<>();
         }
-        Map<Character,String>mp=new HashMap();
+        Map<Character, String>mp=new HashMap<>();
         mp.put('2', "abc");
-        mp.put('3',"def");
+        mp.put('3', "def");
         mp.put('4', "ghi");
         mp.put('5', "jkl");
         mp.put('6', "mno");
-        mp.put('7',"pqrs");
+        mp.put('7', "pqrs");
         mp.put('8', "tuv");
         mp.put('9', "wxyz");
+        StringBuilder sb=new StringBuilder();
+        solve(0,digits, sb, mp);
+        return new ArrayList<>(result);
         
-
-        StringBuilder temp=new StringBuilder();
-        solve(0, digits, temp, mp);
-        return result;
-
+        
     }
 }
