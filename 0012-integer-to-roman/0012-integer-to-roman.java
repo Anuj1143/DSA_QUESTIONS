e0758701872e13581b1831fcf90285784da36117
Anuj Mourya <161029725+Anuj1143@users.noreply.github.com>
2025-06-23 17:12:43 +0530

    Time: 3 ms (98.39%), Space: 44.8 MB (24.53%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.java
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -1,17 +1,16 @@
 class Solution {
     public String intToRoman(int num) {
-        List<Integer> val=Arrays.asList(1000,900,500,400,100,90,50,40,10,9,5,4,1);
-        List<String> sys=Arrays.asList("M","CM","D","CD","C","XC","L","XL","X","IX", "V","IV","I");
+        int[] val={1000, 900, 500, 400, 100,90, 50, 40, 10, 9, 5, 4, 1};
+        String[] sys={"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV","I"};
         StringBuilder result=new StringBuilder();
-        for(int i=0; i<13; i++){
-            if(num==0) break;
-            int time=num/val.get(i);
-            while(time-->0){
-                result.append(sys.get(i));
+        for(int i=0; i<val.length && num>0; i++){
+            while(num>=val[i]){
+                result.append(sys[i]);
+                num-=val[i];
             }
-            num=num%val.get(i);
+
         }
-          return result.toString();
+        return result.toString();
+        
     }
-  
 }
