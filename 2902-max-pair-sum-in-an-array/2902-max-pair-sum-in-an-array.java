bcabcafed7736adc539c5042bbc233acae91f0a6
Anuj Mourya <161029725+Anuj1143@users.noreply.github.com>
2025-06-25 15:40:26 +0530

    Time: 2 ms (100%), Space: 44.6 MB (67.87%) - LeetHub

--- 2902-max-pair-sum-in-an-array/2902-max-pair-sum-in-an-array.java
+++ 2902-max-pair-sum-in-an-array/2902-max-pair-sum-in-an-array.java
@@ -1,32 +1,23 @@
-import java.util.*;
-
 class Solution {
     public int maxSum(int[] nums) {
-        Map<Integer, Integer> maxMap = new HashMap<>();
-        int maxSum = -1;
+        int [] largest=new int[10];
+        int result=-1;
+        for(int num:nums){
+            int max=0;
+            int number=num;
+            while(number>0){
+                int digit=number%10;
+                number/=10;
+               max=Math.max(max, digit);
 
-        for (int num : nums) {
-            int maxDigit = getMaxDigit(num);
 
-            if (maxMap.containsKey(maxDigit)) {
-                // Update max sum
-                maxSum = Math.max(maxSum, maxMap.get(maxDigit) + num);
-                // Update the larger value for this digit group
-                maxMap.put(maxDigit, Math.max(maxMap.get(maxDigit), num));
-            } else {
-                maxMap.put(maxDigit, num);
             }
+            if(largest[max]>0)
+                result=Math.max(result,largest[max]+num);
+            
+             largest[max]=Math.max(largest[max], num);
+            
         }
-
-        return maxSum;
-    }
-
-    private int getMaxDigit(int num) {
-        int maxDigit = 0;
-        while (num > 0) {
-            maxDigit = Math.max(maxDigit, num % 10);
-            num /= 10;
-        }
-        return maxDigit;
+        return result;
     }
 }
