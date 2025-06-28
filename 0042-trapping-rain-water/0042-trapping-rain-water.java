a3f70b25d86c5af4a09a007aaf3c738d87502163
Anuj Mourya <161029725+Anuj1143@users.noreply.github.com>
2025-06-28 12:46:20 +0530

    Time: 1 ms (63.28%), Space: 45.8 MB (92.96%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,26 +1,25 @@
 class Solution {
-    public int trap(int[] arr) {
-        int n=arr.length;
+    public int trap(int[] nums) {
+        int n=nums.length;
         int prefix[]=new int[n];
         int suffix[]=new int[n];
-
-        prefix[0]=arr[0];
+        prefix[0]=nums[0];
         for(int i=1; i<n; i++){
-            prefix[i]=Math.max(arr[i], prefix[i-1]);
+            prefix[i]=Math.max(nums[i], prefix[i-1]);
         }
-        suffix[n-1]=arr[n-1];
+
+        suffix[n-1]=nums[n-1];
         for(int i=n-2; i>=0; i--){
-            suffix[i]=Math.max(arr[i], suffix[i+1]);
+            suffix[i]=Math.max(nums[i], suffix[i+1]);
         }
         int total=0;
-        for(int i=0; i<n; i++){
-        int leftMax=prefix[i];
-        int rightMax=suffix[i];
-        if(arr[i]<leftMax && arr[i]<rightMax){
-            total+=Math.min(leftMax, rightMax)-arr[i];
+        for(int i=0;i<n; i++){
+        int left=prefix[i];
+        int  right=suffix[i];
+        if(nums[i]<left && nums[i]<right){
+            total+=Math.min(left, right )-nums[i];
         }
         }
-
         return total;
     }
 
