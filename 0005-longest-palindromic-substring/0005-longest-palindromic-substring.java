d23f1ef2c3c991787d7eb2badb235d0b04ed7865
Anuj Mourya <161029725+Anuj1143@users.noreply.github.com>
2025-07-04 11:03:26 +0530

    Time: 2026 ms (7.46%), Space: 42.7 MB (56.58%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,8 +1,7 @@
 class Solution {
-    static boolean solve(String s, int i, int j){
+    public boolean solve(String s,int i, int j){
         if(i>=j){
             return true;
-
         }
         if(s.charAt(i)==s.charAt(j)){
             return solve(s, i+1, j-1);
@@ -13,15 +12,16 @@ class Solution {
         int n=s.length();
         int maxLen=Integer.MIN_VALUE;
         int sp=0;
-        for(int i=0; i<n; i++){
-            for(int j=i; j<n; j++){
-                if(solve(s, i, j)==true){
+        for(int i=0;i<n; i++){
+            for(int j=i;j<n; j++){
+                if(solve(s, i ,j)==true){
                     if(j-i+1>maxLen){
                         maxLen=j-i+1;
                         sp=i;
                     }
                 }
             }
+
         }
         return s.substring(sp, sp+maxLen);
     }
